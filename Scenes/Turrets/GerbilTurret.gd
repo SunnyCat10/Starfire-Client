127e938bc0e3c777cf3cc760eb7e89e671dfe963
SunnyCat10 <daniel.dovgun@gmail.com>
2023-10-23 20:03:23 +0300

    Fixed server-side projectiles
    Fixed layer masks with server-side projectiles.
    
    Co-Authored-By: Amouxi <48153396+Amouxi@users.noreply.github.com>

--- Scenes/Turrets/GerbilTurret.gd
+++ Scenes/Turrets/GerbilTurret.gd
@@ -12,5 +12,8 @@ func attack():
 		var projectile_instance : Node2D = projectile.instantiate()
 		projectile_instance.position = muzzle.global_position 
 		projectile_instance.rotation = global_rotation
+		if str(get_parent().name) == "CharacterBody2D":
+			projectile_instance.set_collision_layer(4)
+			projectile_instance.set_collision_mask(9)
+			Server.send_attack(position, rotation)
 		get_tree().get_root().add_child(projectile_instance)
-		Server.send_attack(position, rotation)

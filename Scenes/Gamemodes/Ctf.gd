57a8c02c9bf1e45269cd75e83b760e495731f454
SunnyCat10 <daniel.dovgun@gmail.com>
2023-11-08 21:07:39 +0200

    Added team based respawn
    Added team based respawn based on the server and spawn nodes on the map.

--- Scenes/Gamemodes/Ctf.gd
+++ Scenes/Gamemodes/Ctf.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const FLAGPOLE_IDENTIFIER : String = "Flagpole"
 
+enum Buffer {TIME, PACKET}
+
 var allied_team = {}
 var enemy_team = {}
 
@@ -23,14 +25,17 @@ var timer_ready : bool = false
 
 var flag_list = []
 var status_update_cache = {} # CHANGE THIS TO SOMEKIND OF A LIST!!!!
+var packet_buffer = []
+var indices_to_remove = []
 
-func _ready():
+
+func _ready() -> void:
 	# setup_flags()
 	Server.gamemode_started.connect(setup_game)
 	Packets.gamemode_update.connect(append_status_update)
 
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	if not _starting_time == 0.0 and _starting_time <= Server.client_clock:
 		if timer_ready:
 			ctf_ui.update_time(timer.time_left)
@@ -40,14 +45,26 @@ func _physics_process(delta):
 			setup_ctf_players()
 
 
-	for status in status_update_cache:
-		if status <= Server.client_clock:
-			render_status(status_update_cache[status])
-			# ctf_ui.update_status(status_update_cache[status])
-			status_update_cache.erase(status)
+#	for status in status_update_cache:
+#		if status <= Server.client_clock:
+#			render_status(status_update_cache[status])
+#			# ctf_ui.update_status(status_update_cache[status])
+#			status_update_cache.erase(status)
 
 
-func setup_game(player_list, starting_time : float):
+
+# ctf_ui.update_status(status_update_cache[status])
+
+	var updated_buffer = []
+	for packet in packet_buffer:
+		if packet[Buffer.TIME] <= Server.client_clock:
+			render_status(packet[Buffer.PACKET])
+		else:
+			updated_buffer.append(packet)
+	packet_buffer = updated_buffer
+
+
+func setup_game(player_list, starting_time : float) -> void:
 	var player_id : int = multiplayer.get_unique_id()
 	if (player_list[Packets.CtfTeam.TEAM_A].has(player_id)):
 		allied_team = player_list[Packets.CtfTeam.TEAM_A]
@@ -69,11 +86,11 @@ func setup_game(player_list, starting_time : float):
 	ctf_ui.client_team_id = client_team_id
 
 
-func end_game():
+func end_game() -> void:
 	pass
 
 
-func setup_flags():
+func setup_flags() -> void:
 	var flag_id : int = 0
 	for objective in objectives.get_children():
 		if objective.name.contains(FLAGPOLE_IDENTIFIER):
@@ -86,32 +103,36 @@ func setup_flags():
 			flag_id = flag_id + 1
 
 
-func on_pickup_flag(team_id : int):
+func on_pickup_flag(team_id : int) -> void:
 	ctf_ui.flag_taken(Server.Team.ALLY_TEAM) if client_team_id == team_id else ctf_ui.flag_taken(Server.Team.ENEMY_TEAM)
 
 
-func on_return_flag(team_id : int):
+func on_return_flag(team_id : int) -> void:
 	ctf_ui.flag_returned(Server.Team.ALLY_TEAM) if client_team_id == team_id else ctf_ui.flag_returned(Server.Team.ENEMY_TEAM)
 
 
-func on_capture_flag(team_id : int):
+func on_capture_flag(team_id : int) -> void:
 	ctf_ui.flag_captured(Server.Team.ENEMY_TEAM) if client_team_id == team_id else ctf_ui.flag_captured(Server.Team.ALLY_TEAM)
 
 
-func append_status_update(packet, event_time : float):
-	status_update_cache[event_time] = packet
-
-
-func render_status(packet):
+func append_status_update(packet, event_time : float) -> void:
 	print(packet)
+	# status_update_cache[event_time] = packet
+	packet_buffer.append([event_time, packet])
+
+
+func render_status(packet) -> void:
+	# print(packet)
 	match packet[Packets.PACKET_ID]:
 		Packets.Type.PICKUP_FLAG:
 			render_flag_pickup(packet)
 		Packets.Type.CAPTURE_FLAG:
 			render_flag_capture(packet)
+		Packets.Type.SPAWN_PLAYER:
+			spawn_player(packet)
 
 
-func setup_ctf_players():
+func setup_ctf_players() -> void:
 	for ally_player in allied_team:
 		allied_team[ally_player] = get_parent().get_node(str(ally_player))
 		allied_team[ally_player].flag_manager.setup_manager(client_team_id, client_team_id)
@@ -121,7 +142,7 @@ func setup_ctf_players():
 		enemy_team[enemy_player].flag_manager.setup_manager(enemy_team_id, client_team_id)
 
 
-func render_flag_pickup(packet):
+func render_flag_pickup(packet) -> void:
 	var flag = flag_list[packet[Packets.PickupFlag.FLAG_ID]]
 	var player_id : int = packet[Packets.PickupFlag.PLAYER_ID]
 	flag.pickup_flag()
@@ -131,14 +152,24 @@ func render_flag_pickup(packet):
 		enemy_team[player_id].flag_manager.pickup(flag)
 
 
-func render_flag_capture(packet):
+func render_flag_capture(packet) -> void:
 	var flag = flag_list[packet[Packets.CaptureFlag.FLAG_ID]]
 	var player_id : int = packet[Packets.CaptureFlag.PLAYER_ID]
 	flag.capture_flag()
 	if allied_team.has(player_id):
 		allied_team[player_id].flag_manager.capture_flag()
-	if enemy_team.has(player_id):
+	elif enemy_team.has(player_id):
 		enemy_team[player_id].flag_manager.capture_flag()
+
+
+func spawn_player(packet) -> void:
+	var player_id : int = packet[Packets.SpawnPlayer.PLAYER_ID]
+	var spawn_location : Vector2 = packet[Packets.SpawnPlayer.SPAWN_LOCATION]
+	if allied_team.has(player_id):
+		allied_team[player_id].global_position = spawn_location
+	elif enemy_team.has(player_id):
+		enemy_team[player_id].global_position = spawn_location
+
 
 #func test():
 #	timer = Timer.new()
